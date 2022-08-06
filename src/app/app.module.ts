17571961739e329d851785bfaeb69dd118af73fa
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-08-06 14:10:10 +0200

    add all relevant Moduls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,19 @@ import { AppComponent } from './app.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { OrganisationComponent } from './organisation/organisation.component';
+import { LoginComponent } from './login/login.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { StartComponent } from './start/start.component';
+import { OrganisationDetailComponent } from './organisation-detail/organisation-detail.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { FooterComponent } from './footer/footer.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent, UserComponent, OrganisationComponent, LoginComponent, UserDetailComponent, StartComponent, OrganisationDetailComponent, ImprintComponent, FooterComponent, DataProtectionComponent],
   imports: [
     BrowserModule,
     MatIconModule,
